b2ae95f5431405c973a0217cbf64aa20d3cd2e38
stelian56 <elviestelian@gmail.com>
2026-07-21 19:32:02 -0400

    Version 4.1

--- scripts/makedocs.bat
+++ scripts/makedocs.bat
@@ -12,10 +12,11 @@ for /d %%a in (*) do (
     cd %WORK_DIR%
     echo "Building documentation for: %%a"
     mkdir %DOC_BUILD_DIR%\%%a
+
     xsltproc ^
 	--stringparam html.stylesheet ../../css/doc.css ^
 	--stringparam generate.toc "book toc" ^
     --stringparam section.autolabel "1" ^
 	--stringparam ulink.target "_blank" ^
-	-o %DOC_BUILD_DIR%\%%a\UsersGuide.html "%DOCBOOK_HOME%\html\docbook.xsl" %DOC_DIR%\%%a\UsersGuide.xml
+	-o %DOC_BUILD_DIR%\%%a\UsersGuide.html "%DOCBOOK_HOME%\html\geometria.xsl" %DOC_DIR%\%%a\UsersGuide.xml
 )
---
+++ xsl/geometria.xsl
@@ -0,0 +1,14 @@
+<?xml version='1.0'?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version='1.0'>
+
+<xsl:import href="docbook.xsl"/>
+
+<xsl:output method="html"
+            encoding="UTF-8"
+            indent="yes"/>
+
+<xsl:template name="user.head.content">
+ <meta name="viewport" content="width=device-width,initial-scale=1" />
+</xsl:template>
+
+</xsl:stylesheet>
